9bdb533e4d17edb92116759dd3819ed31bcd6471
Mechanicalgoat <tomato.co.jp.blog@gmail.com>
2025-03-11 22:18:29 +0900

    Create homerun-view.js

---
+++ homerun-view.js
@@ -0,0 +1,9 @@
+// ホームラン飛距離の俯瞰表示機能
+const homeRunViewManager = {
+    // ホームラン情報の履歴
+    homeRuns: [],
+    
+    // ホームラン飛距離の俯瞰表示を初期化
+    init() {
+        // ホームラン表示のためのコンテナを追加
+        const gameContainer = document.querySelector('.
